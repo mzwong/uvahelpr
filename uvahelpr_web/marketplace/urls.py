f10777ca0d73a141b266e3d9e85f6eb02e5f2c8c
Abhishek <ag4cb@virginia.edu>
2016-10-01 15:18:44 -0400

    added job details page for marketplace

--- uvahelpr_web/marketplace/urls.py
+++ uvahelpr_web/marketplace/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 from . import views
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^jobs/(?P<id>[0-9]+)/$', views.job_entry, name="job_entry")
 ]
--- uvahelpr_web/marketplace/views.py
+++ uvahelpr_web/marketplace/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import urllib.request
+import urllib.parse
+import json
 
 # Create your views here.
 def index(request):
 	return render(request, 'marketplace/index.html')
+
+def job_entry(request, id):
+	# make a GET request and parse the returned JSON                                                                                                                                                           # note, no timeouts, error handling or all the other things needed to do this for real
+	req = urllib.request.Request('http://www.mocky.io/v2/57f001943d0000dc1e0dd7ca')
+	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+	resp_dict = json.loads(resp_json)
+	context = {"job": resp_dict["result"]}
+	return render(request, 'marketplace/job_detail.html', context=context)
