6a72da42cc5c1cb4cd707b3354c2aac9fb4be3c7
Maurice Wong <maurice.wong24@gmail.com>
2016-10-01 20:11:37 -0400

    Working individual job entry link. Fix job entry fields in template

--- uvahelpr_exp/uvahelpr_exp/views.py
+++ uvahelpr_exp/uvahelpr_exp/views.py
@@ -6,3 +6,8 @@ def get_all_jobs(request):
     req = urllib.request.Request('http://' + request.META['SERVER_NAME'] + ':8001/api/v1/jobs/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     return HttpResponse(resp_json)
+
+def job_entry(request, id):
+    req = urllib.request.Request('http://' + request.META['SERVER_NAME'] + ':8001/api/v1/jobs/' + id + '/')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    return HttpResponse(resp_json)
--- uvahelpr_web/marketplace/views.py
+++ uvahelpr_web/marketplace/views.py
@@ -10,7 +10,8 @@ def index(request):
 
 def job_entry(request, id):
 	# make a GET request and parse the returned JSON                                                                                                                                                           # note, no timeouts, error handling or all the other things needed to do this for real
-	req = urllib.request.Request('http://www.mocky.io/v2/57f001943d0000dc1e0dd7ca')
+	#req = urllib.request.Request('http://www.mocky.io/v2/57f001943d0000dc1e0dd7ca')
+	req = urllib.request.Request('http://' + request.META['SERVER_NAME'] + ':8002/jobs/'+id + '/')
 	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
 	resp_dict = json.loads(resp_json)
 	context = {"job": resp_dict["result"]}
