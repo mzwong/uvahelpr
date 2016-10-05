335da0db83fb92ba2a39f87d5eca6e71ef17ffc0
will-gerard <whg8hc@virginia.edu>
2016-10-04 21:15:53 -0400

    changed url format

--- uvahelpr_exp/uvahelpr_exp/views.py
+++ uvahelpr_exp/uvahelpr_exp/views.py
@@ -4,7 +4,7 @@ import urllib.parse
 import json
 
 def get_all_jobs(request):
-    req = urllib.request.Request('http://' + request.META['SERVER_NAME'] + ':8001/api/v1/jobs/')
+    req = urllib.request.Request('http://models-api:8000/api/v1/jobs/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp_dict = json.loads(resp_json)
     filter = []
@@ -15,7 +15,7 @@ def get_all_jobs(request):
     return HttpResponse(result)
 
 def job_summary(request, id):
-    req = urllib.request.Request('http://' + request.META['SERVER_NAME'] + ':8001/api/v1/jobs/' + id + '/')
+    req = urllib.request.Request('http://models-api:8000/api/v1/jobs/' + id + '/')
     resp_json = urllib.request.urlopen(req).read().decode('utf-8')
     resp_dict = json.loads(resp_json)
     job = resp_dict['result']
@@ -29,8 +29,9 @@ def job_summary(request, id):
     lastname = user_info['last_name']
     name = firstname + ' ' + lastname
 
-    newjob = {'id': job['id'], 'title': job['title'], 'requester': name, 'description': job['description'], 'time_required': job['time_required'],
-              'event_time': job['event_time'], 'skills_required': job['skills_required'], 'compensation': job['compensation'], 'location':job['location']}
+    newjob = {x: job[x] for x in ['id', 'title', 'description', 'time_required', 'event_time', 'skills_required', 'compensation', 'location']}
+    newjob['requester'] = name
+
     result = json.dumps(newjob)
     return HttpResponse(result)
 
--- uvahelpr_web/marketplace/views.py
+++ uvahelpr_web/marketplace/views.py
@@ -11,7 +11,7 @@ def index(request):
 def job_entry(request, id):
 	# make a GET request and parse the returned JSON                                                                                                                                                           # note, no timeouts, error handling or all the other things needed to do this for real
 	#req = urllib.request.Request('http://www.mocky.io/v2/57f001943d0000dc1e0dd7ca')
-	req = urllib.request.Request('http://' + request.META['SERVER_NAME'] + ':8002/jobs/'+id + '/')
+	req = urllib.request.Request('http://exp-api:8000/jobs/' + id + '/')
 	resp_json = urllib.request.urlopen(req).read().decode('utf-8')
 	resp_dict = json.loads(resp_json)
 	context = {"job": resp_dict}
@@ -19,7 +19,7 @@ def job_entry(request, id):
 
 def allJobs(request):
 	#Experience service call to get all jobs
-	req = urllib.request.Request('http://' + request.META['SERVER_NAME']+':8002/jobs/')
+	req = urllib.request.Request('http://exp-api:8000/jobs/')
 
 	#mock response data for testing:
 	#req = urllib.request.Request('http://www.mocky.io/v2/57efed1a3d0000371e0dd7c4')
