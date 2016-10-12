1ed811be4720c8b9f1ccd4e19f22ed65b7e068d5
Abhishek <ag4cb@virginia.edu>
2016-10-11 20:40:04 -0400

    refactor to incorporate users

--- uvahelpr_models/marketplace/views.py
+++ uvahelpr_models/marketplace/views.py
@@ -1,13 +1,12 @@
 from django.views.generic import View
 from django.http import JsonResponse
-from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
 from django.core.exceptions import ObjectDoesNotExist
 from json import dumps
 from django.forms.models import model_to_dict
 
-from .forms import UserForm, ProfileForm, JobForm, MessageForm
-from .models import Profile, Job, Message
+from .forms import  JobForm, MessageForm, HelprUserForm
+from .models import HelprUser, Job, Message
 
 ############ VIEWS ####################
 
@@ -17,13 +16,9 @@ from .models import Profile, Job, Message
 @require_http_methods(["POST"])
 def create_user(request):
 	result = {}
-	user_form = UserForm(request.POST)
-	profile_form = ProfileForm(request.POST)
-	if user_form.is_valid() and profile_form.is_valid():
+	user_form = HelprUserForm(request.POST)
+	if user_form.is_valid() :
 		user = user_form.save()
-		profile = profile_form.save(commit=False)
-		profile.user = user
-		profile.save()
 		result["ok"] = True
 		result["result"] = {"id": user.id}
 	else:
@@ -37,7 +32,7 @@ def create_user(request):
 def delete_user(request):
 	result = {}
 	try:
-		user = User.objects.get(pk=request.POST["id"])
+		user = HelprUser.objects.get(pk=request.POST["id"])
 		user.delete()
 		result["ok"] = True
 		result["result"] = "User successfully deleted."
@@ -126,11 +121,9 @@ class UserRU(View):
 	def get(self, request, id):
 		result = {}
 		try:
-			user = User.objects.get(pk=id)
+			user = HelprUser.objects.get(pk=id)
 			result["ok"] = True
-			attr_dict = model_to_dict(user, fields=('id', 'username', 'email', 'first_name', 'last_name'))
-			attr_dict.update(model_to_dict(user.profile, fields=Profile.updateable_fields))
-			result["result"] = attr_dict
+			result["result"] = model_to_dict(user)
 		except ObjectDoesNotExist:
 			result["ok"] = False
 			result["result"] = "User does not exist."
@@ -139,16 +132,12 @@ class UserRU(View):
 	def post(self, request, id):
 		result = {}
 		try:
-			user = User.objects.get(pk=id)
-			user_fields = [f.name for f in User._meta.get_fields()]
+			user = HelprUser.objects.get(pk=id)
+			user_fields = [f.name for f in HelprUser._meta.get_fields()]
 			for field in user_fields:
 				if field in request.POST:
 					setattr(user, field, request.POST[field])
-			for field in Profile.updateable_fields:
-				if field in request.POST:
-					setattr(user.profile, field, request.POST[field])
 			user.save()
-			user.profile.save()
 			result["ok"] = True
 			result["result"] = "User updated succesfully."
 		except ObjectDoesNotExist:
