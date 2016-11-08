e4fd24a292c9d0acd208ba11e664cf5eb91088cb
Maurice Wong <maurice.wong24@gmail.com>
2016-11-08 00:43:50 -0500

    Add batch to ES script

--- indexES.py
+++ indexES.py
@@ -1,1 +1,10 @@
-pass
+from kafka import KafkaConsumer
+from elasticsearch import Elasticsearch
+import json
+
+consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+es = Elasticsearch(['es'])
+for listing in consumer:
+    listingjson = json.loads((listing.value).decode('utf-8'))
+    es.index(index='listing_index', doc_type='listing', id=listingjson['id'], body=listing)
+    es.indices.refresh(index="listing_index")
--- uvahelpr_exp/uvahelpr_exp/tests.py
+++ uvahelpr_exp/uvahelpr_exp/tests.py
@@ -5,18 +5,5 @@ import urllib.request
 import urllib.parse
 import json
 
-# class GetJobSummaryTestCase(TestCase):
-#   def setUp(self):     #setUp method is called before each test in this class
-#      setup_test_environment()
-#
-#   def test_success_response(self):
-#      checkout_url = '{}{}'.format(self.live_server_url, reverse('checkout', kwargs={'pk': article.id}))
-#      parsed_url = urlparse(self.live_server_url)
-#      response = self.client.get(checkout_url, SERVER_NAME=parsed_url.hostname, SERVER_PORT=parsed_url.port)
-#      url = reverse('get_job', kwargs={'id':1})
-#      print("Calling {}".format(url))
-#      response = self.client.get(url)   #assumes job with id 1 is stored in db
-#      self.assertEquals(response.status_code, 200)
-#
-#   def tearDown(self):  #tearDown method is called after each test
-#      pass              #nothing to tear down
+
+
